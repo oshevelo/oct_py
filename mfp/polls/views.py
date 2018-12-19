aae766e1667ecd0b3790151bedb9fa3ea37658af
Alex Shevelo <oshevelo@softserveinc.com>
2018-12-19 20:23:10 +0200

    test commit1

--- mfp/polls/views.py
+++ mfp/polls/views.py
@@ -5,6 +5,10 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+
+
+
+
 def list(request):
     return HttpResponse("Hello, world. You're at the polls list.")
 
